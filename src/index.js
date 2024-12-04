4dab7290c4016369a315777be465fa3c67170f10
surbhigoel555 <surbhigoel555@gmail.com>
2024-12-04 11:17:52 +0530

    Initial setup

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAKnOAlduKm1ucE3DMq3HM3VUC-qBT0hfQ",
+  authDomain: "notnotion-2584b.firebaseapp.com",
+  projectId: "notnotion-2584b",
+  storageBucket: "notnotion-2584b.firebasestorage.app",
+  messagingSenderId: "755531495491",
+  appId: "1:755531495491:web:86eaa78a639eac06f37cf6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
